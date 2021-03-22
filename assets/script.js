4ffad937f8c5f9998cd281fe4c677d10dc400a8f
AustinJoo97 <austinjoo97@gmail.com>
2021-03-22 01:47:48 -0400

    created early html elements to render and allow user to search for a city; linked script file to html

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+let citySearch = document.getElementById('searchAndHistory');
+let searchButton = document.getElementById('searchButton');
+let lastFiveSearched = document.getElementById('lastFiveSearched');
