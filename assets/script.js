4d6aeb9823439b36b0353e32be26091fa944503b
AustinJoo97 <austinjoo97@gmail.com>
2021-03-22 02:19:55 -0400

    added additional ids to html elements; created function that, on submit, will take the value of the search box and use that to generate a query that will be attached to the API call

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,33 @@
 let citySearch = document.getElementById('searchAndHistory');
+let searchedCityQuery = document.getElementById('searchedCityQuery');
 let searchButton = document.getElementById('searchButton');
 let lastFiveSearched = document.getElementById('lastFiveSearched');
+let weatherAPI = 'api.openweathermap.org/data/2.5/forecast?q=';
+let tempAPI;
+
+function retrieveWeather(event){
+    event.preventDefault(); 
+    let searchQuery = '';
+    let searchArray = searchedCityQuery.value.split(" ");
+
+    if(searchArray.length < 1){
+        return;
+    } else if(searchArray.length === 1){
+        searchQuery = searchArray[0];
+    } else {
+        for(let i = 0; i < searchArray.length; i++){
+            if(i === searchArray.length-1){
+                searchQuery += `${searchArray[i]}`
+            } else {
+                searchQuery += `${searchArray[i]}+`
+            }
+        }
+    }
+    tempAPI = weatherAPI + searchQuery;
+    // console.log(tempAPI)
+    tempAPI = '';
+}
+
+
+
+citySearch.addEventListener("submit", retrieveWeather)
